f61694beebe7b485f37c1b0009f78877e2ec1ab5
PapaDios0 <109675506+PapaDios0@users.noreply.github.com>
2024-09-25 01:27:46 +0000

    Completed HW

--- index.js
+++ index.js
@@ -8,8 +8,17 @@
 // D. Add a new fruit, "grape", to the end of the array.
 // E. Delete the last fruit in the array.
 // F. Log the updated array.
+let fruits = ["apple", "banana", "cherry"]
 
+console.log(fruits[1])
 
+fruits[2] = "orange"
+
+fruits.push("grape")
+
+fruits.pop()
+
+console.log(fruits)
 
 // TASK 2: Objects - Create, Read, Update, Delete (CRUD)
 // A. Create an object called `student` with the keys: name, age, and grade. (add your own values)
@@ -18,23 +27,40 @@
 // D. Add a new property `isGraduated` and set it to `true`.
 // E. Delete the `age` property from the object.
 // F. Log the updated student object.
+let student = {
+   name: "Christian Rojas",
+   age: 17,
+   grade: "B+"
+}
 
+student.grade = "A+"
 
+student.isGraduated = true
 
+delete student.age
+
+console.log(student)
 // TASK 3: Functions - Create and Call Functions
 // A. Write a function called `calculateTotal` that takes two inputs: price and quantity.
 //    The function should return the total price by multiplying price by quantity.
-
-
-
 // B. Call the `calculateTotal` function 3 times and console log the outputs
 // First with the inputs 20 and 5
 // Second with the inputs 12 and 8
 // Third with the inputs 9 and 42
 
+function calcualteTotal(price, quantity){
+   return price * quantity
+};
+let result = calcualteTotal(20, 5)
+let result2 = calcualteTotal(12, 8)
+let result3 = calcualteTotal(9, 42)
 
+console.log(result)
+console.log(result2)
+console.log(result3)
 /*
    ==== Final Commit Instructions ====
    At this point, commit your work with an appropriate commit message.
    Sync changes to push your commits and submit the homework!
 */
+
